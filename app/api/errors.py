853c4389fe139386665fbfcaa171c6f489c5e5c0
Aleksey Bekoev <bekoev@mad-soft.ru>
2025-05-23 12:55:50 +0300

    [TGC-64] Prevent from automatic version downgrade in manifest

--- app/api/errors.py
+++ app/api/errors.py
@@ -4,11 +4,11 @@ from fastapi import HTTPException, status
 class ApiNotFoundError(HTTPException):
     def __init__(self):
         self.status_code = status.HTTP_404_NOT_FOUND
-        self.detail = "not found"
+        self.detail = "Not found"
 
 
 class WrongDataError(HTTPException):
-    def __init__(self, loc="unk", message="field required"):
+    def __init__(self, loc="unk", message="Field required"):
         self.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
         self.loc = loc
         self.detail = [
@@ -23,4 +23,10 @@ class WrongDataError(HTTPException):
 class ApiUnauthorizedError(HTTPException):
     def __init__(self, message: str | None = None):
         self.status_code = status.HTTP_401_UNAUTHORIZED
-        self.detail = message if message else "unauthorized"
+        self.detail = message if message else "Unauthorized"
+
+
+class ApiForbiddenError(HTTPException):
+    def __init__(self, message: str | None = None):
+        self.status_code = status.HTTP_403_FORBIDDEN
+        self.detail = message if message else "Forbidden"
