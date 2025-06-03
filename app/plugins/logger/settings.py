19a9de223965b699ac769bef492fab7c20facc3d
Aleksey Bekoev <bekoev@mad-soft.ru>
2025-06-03 17:49:39 +0300

    [TGC-64] Add logging to files

--- app/plugins/logger/settings.py
+++ app/plugins/logger/settings.py
@@ -1,6 +1,5 @@
 from enum import StrEnum
 from functools import lru_cache
-from typing import Optional
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -12,9 +11,11 @@ class LoggingLVL(StrEnum):
 
 class LoggerSettings(BaseSettings):
     level: LoggingLVL = LoggingLVL.INFO
-    graylog_host: Optional[str] = None
-    graylog_port: Optional[int] = None
-    developer_logger: Optional[bool] = None
+    developer_logger: bool | None = None
+    file_storage_path: str = "/persistent/log_storage"
+    file_max_size: int = 0
+    file_backup_count: int = 0
+
     model_config = SettingsConfigDict(
         env_prefix="logger_",
         env_file=".env",
